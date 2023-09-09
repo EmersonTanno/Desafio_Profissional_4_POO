94244031ffdd98b4314c660fcc7b78f8c2c4500c
EmersonTanno <141344298+EmersonTanno@users.noreply.github.com>
2023-09-09 17:11:08 -0300

    Add files via upload

--- dp_poo/src/main/java/org/example/Main.java
+++ dp_poo/src/main/java/org/example/Main.java
@@ -16,22 +16,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) throws CpfException {
 
-        Consulta consulta = new Consulta();
-
-            Medico matheus = new Medico(0, "123123", Especializacao.GINECOLOGIA, "085.777,159-06", "Matheus Dedo-Duro",
-                    new Date(2004, 8, 12), "Helicópitero", "O-");
-
-
-        Paciente rubens = new Paciente(0, "085.777,159-06", "Rubens Pereira Junior",
-                new Date(1999, 12, 6), "Indefinido", "A+", new Date(1999, 12, 7));
-
-        Ubs ubs = new Ubs(0, "AAAA", "133412", 12, "Avenida do KRL", "2131432412");
-
-        String retorno = consulta.agendar(1, matheus, rubens, ubs, ModalidadeConsulta.ONLINE, new Date(2023, 9, 6),
-                new Date(2023, 9, 6, 12, 45), new Date(2023, 9, 6, 12, 50),
-                "");
-
-        System.out.println(retorno);
 
     }
 }
